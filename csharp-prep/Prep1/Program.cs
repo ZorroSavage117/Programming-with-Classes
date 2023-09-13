b66a8f5f871d6fc9d43ab07e25ca47b9392b6efa
ZorroSavage117 <zorrosavage117@byui.edu>
2023-09-13 09:59:15 -0600

    prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,26 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        //String interpolation
+        int myInt = 5;
+        Console.WriteLine("my int = " + myInt);
+        Console.WriteLine($"my int = {myInt}");
+
+        //ReadLine
+        Console.WriteLine("What is your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine($"name = {name}");
+
+        //Conditionals
+        if (name == "Arlo")
+        {
+            Console.WriteLine("Hey thats me");
+        }
+        else
+        {
+            Console.WriteLine($"Hi there, {name}");
+        }
+
     }
 }
