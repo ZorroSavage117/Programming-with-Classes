2b2c417fda16a577e19e680acd89ccd3113ab663
ZorroSavage117 <zorrosavage117@byui.edu>
2023-10-20 21:51:47 -0600

    week 6

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,71 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        // Initialize a new ScriptureCollection and WordFilter
+        ScriptureCollection scriptureCollection = new ScriptureCollection();
+        WordFilter wordFilter = new WordFilter();
+
+        // Get a random scripture
+        Scripture randomScripture = scriptureCollection.GetRandomScripture();
+        wordFilter.InitializeMutableWords(randomScripture);
+
+        // Print the original content
+        Console.WriteLine($"Memorize this scripture from {randomScripture.ScriptureHeading()}");
+        DisplayFormattedWords(randomScripture.FormatContent());
+
+        // Ask if the user wants to continue
+        Console.WriteLine("Do you want to continue? (press enter or type Quit)");
+        string userChoice = Console.ReadLine().ToLower();
+
+        // Main loop for memorization
+        bool isRedacting = true;
+        while (isRedacting)
+        {
+            Console.Clear();
+            List<Word> redactedWords = wordFilter.RedactContent();
+
+            // Display the updated content
+            Console.WriteLine($"Here is the updated content: {randomScripture.ScriptureHeading()}");
+            DisplayFormattedWords(redactedWords);
+
+            // Ask if the user wants to continue
+            Console.WriteLine("Do you want to continue? (press enter or type Quit)");
+            userChoice = Console.ReadLine();
+
+            // If the user types no or there are no more words to redact, exit the loop.
+            if (userChoice.ToLower() == "quit" || wordFilter.redactableIndices.Count == 0)
+            {
+                isRedacting = false;
+            }
+        }
+
+        Console.WriteLine("Good job on your scripture memorization practice!");
+    }
+
+    // Display method for Word objects
+    static void DisplayFormattedWords(List<Word> words)
+    {
+        if (words.Count == 0) return;
+
+        string currentReference = words[0].Reference;
+        int currentVerseNumber = words[0].VerseNumber;
+
+        Console.Write($"{currentVerseNumber} ");
+
+        foreach (var word in words)
+        {
+            if (currentVerseNumber != word.VerseNumber)
+            {
+                Console.WriteLine();  // Start a new line for a new verse
+                currentVerseNumber = word.VerseNumber;
+                Console.Write($"{word.VerseNumber} ");
+            }
+            Console.Write($"{word.Content} ");
+        }
+        Console.WriteLine();
     }
 }
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,92 @@
+public class Word
+{
+    public string Reference { get; set; }
+    public int VerseNumber { get; set; }
+    public string Content { get; set; }
+
+    public Word(string reference, int verseNumber, string content)
+    {
+        Reference = reference;
+        VerseNumber = verseNumber;
+        Content = content;
+    }
+}
+
+public class Scripture
+{
+    // Properties
+    public string Book { get; }
+    public int Chapter { get; }
+    public int StartingVerse { get; }
+    public int EndingVerse { get; }
+    public string Content { get; set; }
+    public List<string> Words { get; set; }
+
+    private static Random random = new Random();
+    // Add a new property to your Scripture class
+    public Dictionary<string, bool> RedactedWords { get; set; }
+
+    // Constructor
+    public Scripture(string book, int chapter, int startingVerse, int endingVerse, string content)
+    {
+        Book = book;
+        Chapter = chapter;
+        StartingVerse = startingVerse;
+        EndingVerse = endingVerse;
+        Content = content;
+        Words = content.Split(' ').ToList();
+
+    }
+
+    public string ScriptureHeading()
+    {
+        if (StartingVerse == EndingVerse)
+        {
+            return $"Memorize this scripture from {Book} {Chapter}:{StartingVerse}";
+        }
+        else
+        {
+            return $"Memorize this scripture from {Book} {Chapter}:{StartingVerse}-{EndingVerse}";
+        }
+    }
+
+
+
+
+    // Method to return formatted Scripture
+    public List<Word> FormatContent()
+    {
+        // Update Content based on the Words list.
+        Content = string.Join(" ", Words);
+
+        // Split the Content string into individual verses based on your separator.
+        string[] verses = Content.Split('|');
+
+        // Initialize a list to hold Word objects.
+        List<Word> formattedContent = new List<Word>();
+
+        // Loop through the verses and append verse numbers.
+        for (int i = 0; i < verses.Length; i++)
+        {
+            // Calculate the current verse number based on StartingVerse.
+            int currentVerseNumber = StartingVerse + i;
+
+            // Get the reference for the current verse.
+            string reference = $"{Book} {Chapter}:{currentVerseNumber}";
+
+            // Split each verse into words.
+            string[] wordsInVerse = verses[i].Trim().Split(' ');
+
+            // Create a Word object for each word and add it to the list.
+            foreach (string word in wordsInVerse)
+            {
+                formattedContent.Add(new Word(reference, currentVerseNumber, word));
+            }
+        }
+
+        return formattedContent;
+    }
+
+
+
+}
