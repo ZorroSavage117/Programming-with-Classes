4cc1311079da3e8da68179112f877a616a118054
ZorroSavage117 <zorrosavage117@byui.edu>
2023-11-08 09:20:11 -0700

    learing

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 5.0));
+        shapes.Add(new Rectangle("Blue", 4.0, 6.0));
+        shapes.Add(new Circle("Green", 3.0));
+
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"Shape color: {shape.GetColor()}");
+            Console.WriteLine($"Shape area: {shape.GetArea()}");
+            Console.WriteLine();
+        }
     }
 }
